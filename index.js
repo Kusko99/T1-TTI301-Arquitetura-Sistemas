c43bd242cca4338d9eae7aff38c920aabd82bc49
Kusko99 <fernandaofo120@gmail.com>
2024-09-14 15:07:23 -0300

    feat(t1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const dotenv = require("dotenv")
 dotenv.config()
 const API_KEY = process.env.API_KEY
 
+//constantes
+//Cidade desajada
+const cidade = "São Paulo"
+//Unidade de medida
+const units = "metric"
+//idoma
+const lang = "pt_br"
 
 //Função que busca as coordenadas
-const obterCoordenadas = async (cidade) => {
+const obterCoordenadas = async(cidade) => {
     try{
-       const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=${1}&appid=${API_KEY}`);
-       const dados = response.data[0];
+       const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=${1}&appid=${API_KEY}`)
+       const dados = response.data[0]
 
        if(dados){
         const {lat, lon} = dados
@@ -27,5 +34,35 @@ const obterCoordenadas = async (cidade) => {
     }
 }
 
+//Função que obtem as condições atuais em função de latitude e longitude
+const obterClima = async (lat, lon) => {
+    try{
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=${lang}&units=${units}`)
+    const dados = response.data
+        if (dados){
+            let description = dados["weather"][0]["description"]
+            fell_like = dados["main"]["feels_like"]
+            console.log(`Clima atual: ${description}, com uma temperatura de ${fell_like} graus.`)
+            return {description, fell_like}
+        }
+        else{
+            console.log(`Não foram encontradas dados do clima para ${lat}, ${lon}.`)
+            return null
+        }
+    }
+    catch (error){
+        console.log("Erro ao buscar dados do clima:", error)
+    }
+}
 
-obterCoordenadas('São Paulo');
+
+
+
+//Main do programa
+(async () => {
+    const coordenadas = await obterCoordenadas(cidade);
+    if (coordenadas) {
+      const { lat, lon } = coordenadas;
+      await obterClima(lat, lon);
+    }
+  })();
